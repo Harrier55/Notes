920fab1f4e901971158bddc52dc5ff9799052d37
Harrier55 <harrier55@yandex.ru>
2021-09-30 00:08:50 +0500

    add ActionBar + onClickOptionMenu add Repo add class NoteEntity add interface NoteRepo

--- app/src/main/java/ru/project/notes/MainActivity.java
+++ app/src/main/java/ru/project/notes/MainActivity.java
@@ -1,14 +1,45 @@
 package ru.project.notes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    ActionBar actionBar;
+    private NotesRepo notesRepo = new NotesRepoImpl();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_note,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        item.getItemId();
+        Toast.makeText(this, "id" + item.getItemId(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+
     }
 }
