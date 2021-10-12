6abd8cbbc88fa7cd697a3871766e57cff1ddf069
Harrier55 <harrier55@yandex.ru>
2021-10-12 20:39:24 +0500

    create initBottomNavigation()

--- app/src/main/java/ru/project/notes/MainActivity.java
+++ app/src/main/java/ru/project/notes/MainActivity.java
@@ -1,44 +1,62 @@
 package ru.project.notes;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.view.menu.ActionMenuItem;
-import androidx.appcompat.view.menu.MenuItemImpl;
-import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 
 import android.annotation.SuppressLint;
-import android.app.Application;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
 
-import static androidx.appcompat.app.ActionBar.DISPLAY_HOME_AS_UP;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
-public class MainActivity extends AppCompatActivity implements OnFragmentClickHandler{
+public class MainActivity extends AppCompatActivity implements OnFragmentClickHandler {
 
     private static final String TAG = "@@@@@ Main Activity";
 
-    private  NotesRepoImpl notesRepo = new NotesRepoImpl();
+    private NotesRepoImpl notesRepo = new NotesRepoImpl();
 
     MainListFragment mainListFragment = new MainListFragment();
     NewListItemFragment newListItemFragment = new NewListItemFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
+    SettingFragment settingFragment = new SettingFragment();
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_for_fragment);
+        initBottomNavigation();
         generateTestRepo();
-        launcherFragment(mainListFragment);
+        launcherFragmentWithBackStack(mainListFragment);
+    }
+
+    private void initBottomNavigation() {
+        bottomNavigationView = findViewById(R.id.bottom_navi);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_item_button_navi_profile:
+                        launchFragmentFromBottomNavigation(profileFragment);
+                        break;
+                    case R.id.menu_item_button_navi_setting:
+                        launchFragmentFromBottomNavigation(settingFragment);
+                        break;
+                    default:
+                        launchFragmentFromBottomNavigation(mainListFragment);
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -48,14 +66,21 @@ public class MainActivity extends AppCompatActivity implements OnFragmentClickHa
         return super.onCreateOptionsMenu(menu);
     }
 
+    public void launchFragmentFromBottomNavigation(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+
     @Override
     public void onClickButtonSaveNoteEditFragment(NoteEntity editNoteEntity) {
-        int idEdit= editNoteEntity.getId();
-        NoteEntity noteEntityEdit = new NoteEntity(editNoteEntity.getTitle(),editNoteEntity.getDetail());
-        notesRepo.updateNote(idEdit,noteEntityEdit);
+        int idEdit = editNoteEntity.getId();
+        NoteEntity noteEntityEdit = new NoteEntity(editNoteEntity.getTitle(), editNoteEntity.getDetail());
+        notesRepo.updateNote(idEdit, noteEntityEdit);
 
         setMainActionBar();
-        launcherFragment(mainListFragment);
+        launcherFragmentWithBackStack(mainListFragment);
     }
 
     @SuppressLint("RestrictedApi")
@@ -63,18 +88,18 @@ public class MainActivity extends AppCompatActivity implements OnFragmentClickHa
     public void onClickItemListNote(NoteEntity noteEntity) {
         NoteEditFragment noteEditFragment = new NoteEditFragment();
         Bundle bundle = new Bundle();
-        bundle.putParcelable("one",noteEntity);
+        bundle.putParcelable("one", noteEntity);
         noteEditFragment.setArguments(bundle);
 
         setCustomActionBar(R.string.edit_note);
-        launcherFragment(noteEditFragment);
+        launcherFragmentWithBackStack(noteEditFragment);
     }
 
     @Override
     public void onClickButtonSaveNewListItemFragment(NoteEntity noteEntity) {
         notesRepo.createNote(noteEntity);
         setMainActionBar();
-        launcherFragment(mainListFragment);
+        launcherFragmentWithBackStack(mainListFragment);
     }
 
     /***
@@ -85,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentClickHa
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         setCustomActionBar(R.string.new_note);
-        launcherFragment(newListItemFragment);
+        launcherFragmentWithBackStack(newListItemFragment);
         return super.onOptionsItemSelected(item);
     }
 
@@ -95,34 +120,36 @@ public class MainActivity extends AppCompatActivity implements OnFragmentClickHa
         setMainActionBar();
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        if(fragmentManager.getBackStackEntryCount() != 0){
-            fragmentManager.popBackStack();}
-        else {super.onBackPressed();}
+        if (fragmentManager.getBackStackEntryCount() != 0) {
+            fragmentManager.popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
-    void launcherFragment(Fragment classFragment){
+    void launcherFragmentWithBackStack(Fragment classFragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
-        if(classFragment != mainListFragment){
+        if (classFragment != mainListFragment) {
 
-        fragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, classFragment)
-                .addToBackStack(null)
-                .commit();}
-        else {
+            fragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, classFragment)
+                    .addToBackStack(null)
+                    .commit();
+        } else {
             fragmentManager.beginTransaction()
                     .replace(R.id.fragment_container, classFragment)
                     .commit();
         }
     }
 
-    void setCustomActionBar(int titleFragment){
+    void setCustomActionBar(int titleFragment) {
         androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(titleFragment);
         View menuItem = findViewById(R.id.addNote);
         menuItem.setVisibility(View.INVISIBLE);
     }
 
-    void setMainActionBar(){
+    void setMainActionBar() {
         androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle(R.string.app_name);
         View menuItem = findViewById(R.id.addNote);
