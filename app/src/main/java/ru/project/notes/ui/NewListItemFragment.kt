2d1f733da3504b4520f7b73232d2a99aaa3df0ff
Harrier55 <harrier55@yandex.ru>
2021-10-27 22:19:19 +0500

    translate NewListItemFragment.kt to Kt

--- app/src/main/java/ru/project/notes/ui/NewListItemFragment.kt
+++ app/src/main/java/ru/project/notes/ui/NewListItemFragment.kt
@@ -1,57 +1,52 @@
-package ru.project.notes.ui;
+package ru.project.notes.ui
 
-import android.content.Context;
-import android.os.Bundle;
+import android.content.Context
+import android.widget.EditText
+import ru.project.notes.listener.OnFragmentClickHandler
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import ru.project.notes.R
+import ru.project.notes.Entity.NoteEntity
+import java.lang.RuntimeException
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-
-import ru.project.notes.Entity.NoteEntity;
-import ru.project.notes.listener.OnFragmentClickHandler;
-import ru.project.notes.R;
-
-public class NewListItemFragment extends Fragment {
-
-    private OnFragmentClickHandler onFragmentClickHandler;
-
-    @Override
-    public void onAttach(@NonNull Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentClickHandler){
-            onFragmentClickHandler =(OnFragmentClickHandler) context;
-        }  else {throw new RuntimeException(context.toString()+ " must implement OnFragmentClickHandler to NewListItemFrag");}
+class NewListItemFragment : Fragment() {
+    private var titleEditText: EditText? = null
+    private var detailEditText: EditText? = null
+    private var saveButton: Button? = null
+    private var onFragmentClickHandler: OnFragmentClickHandler? = null
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        onFragmentClickHandler = if (context is OnFragmentClickHandler) {
+            context
+        } else {
+            throw RuntimeException("$context must implement OnFragmentClickHandler to NewListItemFrag")
+        }
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_note_edit, container, false);
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_note_edit, container, false)
+        initView(view)
+        saveButton!!.setOnClickListener { view1: View? ->
+            val title = titleEditText!!.text.toString()
+            val detail = detailEditText!!.text.toString()
+            val noteEntityNew = NoteEntity(title, detail)
+            onFragmentClickHandler!!.onClickButtonSaveNewListItemFragment(noteEntityNew)
+        }
+        return view
+    }
 
-        EditText titleEditText = view.findViewById(R.id.title_edit_text);
-        EditText detailEditText = view.findViewById(R.id.detail_edit_text);
-        Button saveButton = view.findViewById(R.id.save_button);
-
-        saveButton.setOnClickListener(view1 -> {
-
-            String title = titleEditText.getText().toString();
-            String detail = detailEditText.getText().toString();
-
-            NoteEntity noteEntityNew = new NoteEntity(title,detail);
-
-            onFragmentClickHandler.onClickButtonSaveNewListItemFragment(noteEntityNew);
-        });
-        return view;
-
+    private fun initView(view: View) {
+        titleEditText = view.findViewById(R.id.title_edit_text)
+        detailEditText = view.findViewById(R.id.detail_edit_text)
+        saveButton = view.findViewById(R.id.save_button)
     }
 }
